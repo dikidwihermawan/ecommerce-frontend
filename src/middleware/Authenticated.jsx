d124afca2f6e3dca985fbc0a2908139568aa2dc5
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2023-09-30 13:02:22 +0700

    add middleware

---
+++ src/middleware/Authenticated.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { authenticated } from "../store";
+import { useNavigate } from "react-router-dom";
+
+function Authenticated({ children }) {
+  const redirect = useNavigate();
+  const auth = useRecoilValue(authenticated);
+
+  useEffect(() => {
+    if (!auth.check) {
+      redirect("/login");
+    }
+  }, [auth.check]);
+  return children;
+}
+
+export default Authenticated;
